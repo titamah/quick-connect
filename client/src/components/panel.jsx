16888afae919e8fa7577709854f20fd8dbc5eaa5
Titamah <titamah@icloud.com>
2025-03-04 17:56:45 -0500

    rsizing bottom panel

--- client/src/components/panel.jsx
+++ client/src/components/panel.jsx
@@ -8,11 +8,8 @@ import { Resizable } from 'react-resizable';
 function Panel({ isOpen, setIsOpen, panelSize, setPanelSize }) {
 const panelRef = useRef(null);
 
-const [width, setWidth] = useState(panelSize.width);
-
 const onResize = (event, {size}) => {
-  setWidth(size.width);
-  setPanelSize({width:width, height:panelSize.height});
+  setPanelSize({width:size.width, height:size.height});
 };
 
   useEffect(() => {
@@ -30,12 +27,15 @@ const onResize = (event, {size}) => {
     <>
   <Resizable
   className="duration-100"
-    width={width}
+    width={panelSize.width}
     height={0}
+    minConstraints={[100,0]}
+    onResize={onResize}
+    resizeHandles={['e']}
     handle={
       <div className="fixed z-150 right-0 top-0 h-full w-[10px] cursor-col-resize">
-        <div className="fixed right-[2.5px] top-1/2 cursor-pointer z-200">
-            <span className="absolute start-1/1 -translate-x-1/2 block w-5 h-7 flex items-center bg-white border border-gray-200 text-gray-400 rounded-md  hover:bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-600 dark:hover:bg-neutral-900"
+        <div className="fixed right-0 top-1/2 cursor-pointer z-200">
+            <span className="absolute start-1/2 -translate-x-1/2 block w-5 h-7 flex items-center bg-white border border-gray-200 text-gray-400 rounded-md  hover:bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-600 dark:hover:bg-neutral-900"
             onClick={togglePanel}
             >
               <svg
@@ -60,18 +60,14 @@ const onResize = (event, {size}) => {
             </span>
           </div>
       </div>
-    }
-
-    minConstraints={[100,0]}
-    onResize={onResize}
-    resizeHandles={['e']}>
+    }>
       <div
         id="side-panel"
         ref={panelRef}
         className={`hs-overlay fixed max-sm:hidden [--body-scroll:true] transition-[left] duration-350 transform h-full z-100 bg-white shadow-[3px_0_8px_1px_rgba(0,0,0,0.075)] dark:bg-neutral-800 dark:border-neutral-700`}
         style={{
-          left: isOpen ? 0 : `${width * -1}px`,
-          width: `${width}px`,
+          left: isOpen ? 0 : `${panelSize.width * -1}px`,
+          width: `${panelSize.width}px`,
         }}
         role="dialog"
         aria-labelledby="hs-offcanvas-example-label"
@@ -92,9 +88,45 @@ const onResize = (event, {size}) => {
         </div>
       </div>
       </Resizable>
+  <Resizable
+  className="duration-100"
+    width={0}
+    height={panelSize.height}
+    minConstraints={[0,100]}
+    onResize={onResize}
+    resizeHandles={['n']}
+    handle={
+      <div className="fixed z-150 right-0 top-0 h-[10px] w-full cursor-row-resize"> 
+      <div className="cursor-pointer z-200"
+          onClick={togglePanel}>
+      <span className="absolute start-1/2 -translate-x-1/2 -translate-y-1/2 block w-7 h-5 flex justify-center items-center bg-white border border-gray-200 text-gray-400 rounded-md hover:bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-600 dark:hover:bg-neutral-900">
+        <svg
+          className="shrink-0 size-4.5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="5" cy="8" r="1" />
+          <circle cx="12" cy="8" r="1" />
+          <circle cx="19" cy="8" r="1" />
+          <circle cx="5" cy="16" r="1" />
+          <circle cx="12" cy="16" r="1" />
+          <circle cx="19" cy="16" r="1" />
+        </svg>
+      </span>
+    </div>
+    </div>
+    }
+    >
       <div
         id="bottom-panel"
-        className={`hs-overlay min-sm:hidden fixed [--body-scroll:true] transition-all duration-375 transform w-screen z-100 bg-white shadow-[0_-1px_5px_0px_rgba(0,0,0,0.1)] rounded-t-2xl
+        className={`hs-overlay min-sm:hidden fixed [--body-scroll:true] transition-bottom duration-375 transform w-screen z-100 bg-white shadow-[0_-1px_5px_0px_rgba(0,0,0,0.1)] rounded-t-2xl
 dark:bg-neutral-800 dark:border-neutral-700`}
         style={{
           bottom: isOpen ? 0 : `${panelSize.height * -1}px`,
@@ -103,35 +135,6 @@ dark:bg-neutral-800 dark:border-neutral-700`}
         role="dialog"
         aria-labelledby="hs-offcanvas-example-label"
       >
-        <button
-          type="button"
-          className="w-full inline-flex items-center fixed"
-          onClick={togglePanel}
-        >
-          <div>
-            <span className="absolute top-1/2 start-1/2 -translate-x-1/2 -translate-y-1/2 block w-7 h-5 flex justify-center items-center bg-white border border-gray-200 text-gray-400 rounded-md cursor-col-resize hover:bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-600 dark:hover:bg-neutral-900">
-              <svg
-                className="shrink-0 size-4.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <circle cx="5" cy="8" r="1" />
-                <circle cx="12" cy="8" r="1" />
-                <circle cx="19" cy="8" r="1" />
-                <circle cx="5" cy="16" r="1" />
-                <circle cx="12" cy="16" r="1" />
-                <circle cx="19" cy="16" r="1" />
-              </svg>
-            </span>
-          </div>
-        </button>
         <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
           <h3
             id="hs-offcanvas-example-label"
@@ -147,6 +150,7 @@ dark:bg-neutral-800 dark:border-neutral-700`}
           </p>
         </div>
       </div>
+      </Resizable>
     </>
   );
 }
