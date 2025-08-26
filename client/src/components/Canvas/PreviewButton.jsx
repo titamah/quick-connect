936ca55f974b0fb079aedbf074f7749d3613b2ee
Titamah <titamah@icloud.com>
2025-08-26 15:05:00 -0400

    style undo redo preview buttons

--- client/src/components/Canvas/PreviewButton.jsx
+++ client/src/components/Canvas/PreviewButton.jsx
@@ -1,5 +1,5 @@
 import { forwardRef } from "react";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeClosed } from "lucide-react";
 import { usePreview } from "../../contexts/PreviewContext";
 
 const PreviewButton = forwardRef(({}, ref) => {
@@ -7,20 +7,20 @@ const PreviewButton = forwardRef(({}, ref) => {
 
   return (
     <div
-      className="w-fit flex-col place-items-end right-5 absolute top-2 z-100"
+      className="w-fit place-items-end right-5 absolute top-5 z-100"
     >
       <button
         type="button"
-        className="py-2 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-gray-600 text-white hover:bg-gray-700 focus:outline-hidden focus:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
+        className="py-1.75 px-2 h-fit inline-flex items-center gap-x-2 text-sm font-medium rounded-4xl text-white bg-[var(--accent)] hover:opacity-75 cursor-pointer"
         onClick={togglePreview}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
       >
-        {isPreviewVisible ? "Hide Preview" : "Preview"}
+        {isPreviewVisible ? "Preview" : "Preview"}
         {isPreviewVisible ? (
-          <EyeOff size={16} />
-        ) : (
           <Eye size={16} />
+        ) : (
+          <EyeClosed size={16} />
         )}
       </button>
       
--- client/src/components/Canvas/UndoRedoButton.jsx
+++ client/src/components/Canvas/UndoRedoButton.jsx
@@ -17,36 +17,34 @@ const UndoRedoButton = forwardRef(({}, ref) => {
   };
 
   return (
-    <div className="w-fit flex-col place-items-start left-5 absolute top-2 z-100">
+    <div className="w-fit place-items-end left-5 absolute top-5 z-100">
       <div className="flex gap-2">
         <button
           type="button"
-          className={`py-2 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-white focus:outline-hidden disabled:opacity-50 disabled:pointer-events-none ${
+          className={`py-2 px-2 h-fit inline-flex items-center gap-x-2 text-sm font-medium rounded-4xl text-white hover:opacity-75 ${
             canUndo 
-              ? "bg-gray-600 hover:bg-gray-700 focus:bg-gray-700" 
-              : "bg-gray-400 cursor-not-allowed"
+              ? "bg-[var(--accent)] cursor-pointer" 
+              : "bg-black/50 cursor-not-allowed"
           }`}
           onClick={handleUndo}
           disabled={!canUndo}
           title="Undo"
         >
-          <Undo2 size={16} />
-          Undo
+          <Undo2 size={18} />
         </button>
         
         <button
           type="button"
-          className={`py-2 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-white focus:outline-hidden disabled:opacity-50 disabled:pointer-events-none ${
-            canRedo 
-              ? "bg-gray-600 hover:bg-gray-700 focus:bg-gray-700" 
-              : "bg-gray-400 cursor-not-allowed"
+          className={`py-2 px-2 h-fit inline-flex items-center gap-x-2 text-sm font-medium rounded-4xl text-white hover:opacity-75 ${
+            canUndo 
+              ? "bg-[var(--accent)] cursor-pointer" 
+              : "bg-black/50 cursor-not-allowed"
           }`}
           onClick={handleRedo}
           disabled={!canRedo}
           title="Redo"
         >
-          <Redo2 size={16} />
-          Redo
+          <Redo2 size={18} />
         </button>
       </div>
     </div>
