0676e4e34a14d4a1ffcbe9060fb875a9d4a7ec9f
Titamah <titamah@icloud.com>
2025-09-02 04:58:30 -0400

    landing page responsive

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -36,12 +36,12 @@ const LandingPage = () => {
   ];
 
   return (
-    <main className="flex flex-col h-fit items-start relative bg-[#f5f3e9]">
+    <main className="flex flex-col h-fit w-[100vw] overflow-x-hidden items-start relative bg-[#f5f3e9]">
       
 
-      <section className="flex flex-row flex-wrap h-fit min-h-[80vh] w-full items-center justify-center space-y-[15px] gap-[25px] px-[50px] pb-[50px] pt-[50px] relative self-stretch bg-[var(--brand-orange)] rounded-[0px_0px_90px_90px]">
+      <section className="flex z-2 flex-row flex-wrap-reverse h-fit min-h-[80vh] w-full items-center justify-center space-y-[15px] gap-[25px] px-[50px] pb-[50px] pt-[50px] relative self-stretch bg-[var(--brand-orange)] rounded-[0px_0px_50px_50px] sm:rounded-[0px_0px_90px_90px]">
         <div
-          className="relative max-w-[450px] w-full aspect-[1] !bg-pink-500"
+          className={` ${isMobile ? "hidden" : ""} relative max-w-[450px] w-full aspect-[1] !bg-pink-500"`}
           alt="Custom QR code wallpaper example showing a phone with a personalized QR code design"
           // src={image3}
         >
@@ -64,32 +64,49 @@ const LandingPage = () => {
           </button>
         </div>
       </section>
-
-      <section className="flex flex-col mt-[150px] h-fit min-h-[90vh] w-full items-start gap-[35px] p-[50px] relative self-stretch w-full rounded-[0px_0px_90px_90px] bg-variable-collection-color">
-        <text className="relative w-fit mt-[-1.00px] slab font-black text-[#001d28] text-6xl tracking-[0] leading-[normal] whitespace-nowrap">
-          How It Works
-        </text>
-
-        <div className="flex-wrap items-center gap-[15px] flex-1 self-stretch w-full grow flex relative">
+              <div
+          className={` ${isMobile ? "z-1 mt-[-45px]" : "hidden"} w-full aspect-[1] !bg-pink-500`}
+          alt="Custom QR code wallpaper example showing a phone with a personalized QR code design"
+          // src={image3}
+        >
+        </div>
+      
+      <section className="flex flex-col mt-[50px] h-fit min-h-[90vh] items-center justify-center w-full gap-[35px] p-[25px] sm:p-[50px] relative !space-y-[50px]">
+        {/* <div className="h-full"> */}
+           <p className=" flex-shrink-0 relative w-full max-w-[1200px] mx-auto slab font-black text-[#001d28] self-start sm:text-6xl text-5xl tracking-[0] leading-[normal] whitespace-nowrap">
+            How It Works
+          </p>
+          <span className="flex-wrap gap-[15px] max-w-[1200px] h-full mx-auto w-full flex relative grow">
+           
           {howItWorksSteps.map((step) => (
             <article
               key={step.id}
-              className={`flex flex-col min-w-[415px] min-h-[225px] max-h-[600px] items-start ${step.id === 2 ? "justify-center" : step.id === 3 ? "justify-end" : ""} gap-[15px] p-7.5 pr-9 relative flex-1 self-stretch grow ${step.bgColor}`}
+              className={`flex flex-col min-w-[375px] min-h-[225px] max-h-[600px] items-start self-stretch ${
+                step.id === 2
+                  ? "justify-center"
+                  : step.id === 3
+                  ? "justify-end"
+                  : ""
+              } gap-[10px] sm:gap-[15px] p-5 pr-6 sm:p-7.5 sm:pr-9 relative flex-1 self-stretch grow ${
+                step.bgColor
+              }`}
             >
-              <text className="relative self-stretch mt-[-1.00px] rubik font-black text-[#001d28] text-4xl">
+              <text className="relative self-stretch mt-[-1.00px] rubik font-black text-[#001d28] text-3xl sm:text-4xl">
                 {step.title}
               </text>
 
-              <p className="relative self-stretch font-bold text-neutral-900 text-3xl tracking-[0] leading-[normal]">
+              <p className="relative self-stretch font-bold text-neutral-900 text-xl sm:text-3xl tracking-[0] leading-[normal]">
                 {step.description}
               </p>
             </article>
           ))}
-        </div>
+          </span>
+        {/* </div> */}
       </section>
-      <section className="flex flex-wrap mt-[150px] justify-center gap-[102px_102px] px-0 py-[150px] self-stretch w-full h-fit min-h-[90vh] flex-[0_0_auto] bg-[#001d28] rounded-[90px_90px_0px_0px] items-center relative">
+      
+      <section className="flex flex-wrap mt-[150px] justify-center gap-[102px_102px] px-5 py-[150px] self-stretch w-full h-fit min-h-[90vh] flex-[0_0_auto] bg-[#001d28] rounded-[50px_50px_0px_0px] sm:rounded-[0px_90px_0px_0px] items-center relative">
         <div className="inline-flex flex-col items-center justify-center gap-[75px] px-[33px] py-0 relative flex-[0_0_auto]">
-          <p className="relative w-[653px] rubik-mono font-normal text-white text-5xl text-justify tracking-[0] leading-[normal]">
+          <p className={`relative w-full tracking-tighter max-w-[385px] sm:max-w-[555px] md:max-w-[653px] rubik-mono font-normal text-white text-3xl sm:text-4xl md:text-5xl text-justify tracking-[0] leading-[normal]`}>
             Because business cards get lost and you're way more interesting than
             a black and white square.
           </p>
