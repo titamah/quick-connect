e58ea411918bd687c138d3fe27c932b09f3b8116
Titamah <titamah@icloud.com>
2025-09-23 01:40:45 -0400

    resixing delay

--- client/src/components/Panel/CustomResizable.jsx
+++ client/src/components/Panel/CustomResizable.jsx
@@ -178,7 +178,9 @@ const CustomResizable = forwardRef(
         style={{
           width: isSide ? `${panelSize.width}px` : "100%",
           height: isSide ? "100%" : `${panelSize.height}px`,
-          transition: isSide ? "width 300ms ease-in-out" : "height 300ms ease-in-out",
+          transition: resizeStateRef.current.isResizing 
+            ? "none" 
+            : (isSide ? "width 300ms ease-in-out" : "height 300ms ease-in-out"),
           ...style,
         }}
       >
